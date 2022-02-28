3b22ee343dfcb618864dce5a74d74457e1175eb2
Opilane TTHK <opilane@tthk.ee>
2022-02-28 10:05:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassi_loomine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            inimene naine = new inimene("Kask");
+            Console.WriteLine("Kui vana sa oled? ");
+            naine.Vanus = int.Parse(Console.ReadLine());
+            naine.Tervitamine();
+
+            inimene mees = new inimene();
+            mees.Perenimi = "Anton";
+            mees.Vanus = 17;
+            Console.WriteLine(mees.Perenimi + " on " + mees.Vanus + " aastane mees ");
+            mees.Tervitamine();
+
+            inimene[] inimesed = new inimene[3];
+            for (int i = 0; i < 3; i++)
+            {
+                inimesed[i] = new inimene();
+                Console.WriteLine("Mis on sinu nimi? ");
+                inimesed[i]. Perenimi=Console.ReadLine();
+                Console.WriteLine("Kui vana sa oled? ");
+                inimesed[i]. Vanus = int.Parse(Console.ReadLine());
+            }
+            foreach (inimene inimene in inimesed)
+            {
+                inimene.Tervitamine();
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ inimene.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Klassi_loomine
+{
+    class inimene
+    {
+        string perenimi;
+        public string status;
+        public int vanus;
+        public int palk;
+        public inimene() { }
+        public inimene(string Perenimi)
+        {
+            perenimi = Perenimi;
+        }
+        public string Perenimi 
+        {
+            set { if (perenimi == null) perenimi = value; }
+            get { return perenimi; }
+        }
+        public int Vanus
+        {
+            set 
+            {
+                vanus = value;
+                if (vanus < 7)
+                {
+                    status = "laps";
+                }
+                else if (vanus<17)
+                {
+                    status = "koolilaps";
+                }
+                else if (vanus<24)
+                {
+                    status = "ülikoolilaps";
+                }
+                else
+                {
+                    status = "tööline";
+                }
+            }
+            get { return vanus; }
+        }
+        public string Status
+        {
+            get { return status; }
+        }
+        public void Tervitamine()
+        {
+            Console.WriteLine("Tere! Minu perenimi on "+ perenimi);
+            Console.WriteLine("Ma olen {0} aastat vana, olen {1}", vanus, status);
+        }
+    }
+}
