49f68a48789a5dc01a50a40d138ebc9b46d5544c
Anton Prokhorov <93324290+AntonProkhorov397@users.noreply.github.com>
2022-03-02 13:38:52 +0200

    Update inimene.cs

--- inimene.cs
+++ inimene.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,14 +6,19 @@ using System.Threading.Tasks;
 
 namespace Klassi_loomine
 {
-    class inimene
+    class Inimene
     {
         string perenimi;
         public string status;
         public int vanus;
         public int palk;
-        public string nimi;
+        public string eesnimi;
         public string status1;
+        Emakeel emakeel;
+        public Emakeel Emakeel
+        {
+            get { return emakeel;  }
+        }
         public int Palk
         {
             set
@@ -39,20 +44,21 @@ namespace Klassi_loomine
             get { return palk; }
 
         }
-        public inimene() { }
-        public inimene(string Perenimi)
+        public Inimene() { }
+        public Inimene(string Perenimi, Emakeel emakeel)
         {
             perenimi = Perenimi;
+            this.emakeel = emakeel;
         }
-        public inimene(string Nimi, string Perenimi)
+        public Inimene(string Nimi, string Perenimi)
         {
-            nimi = Nimi;
+            eesnimi = Nimi;
             perenimi = Perenimi;
         }
         public string Nimi
         {
-            set { nimi = value; }
-            get { return nimi; }
+            set { eesnimi = value; }
+            get { return eesnimi; }
 
         }
 
@@ -67,33 +73,57 @@ namespace Klassi_loomine
             set 
             {
                 vanus = value;
-                if (vanus < 7)
-                {
-                    status = "laps";
-                }
-                else if (vanus<17)
-                {
-                    status = "koolilaps";
-                }
-                else if (vanus<24)
-                {
-                    status = "ülikoolilaps";
-                }
-                else
-                {
-                    status = "tööline";
-                }
+                
             }
             get { return vanus; }
         }
         public string Status
         {
-            get { return status; }
+            get {
+            
+                 if (vanus < 7)
+                 {
+                     status = "laps";
+                 }
+                 else if (vanus < 17)
+                 {
+                     status = "koolilaps";
+                 }
+                 else if (vanus < 24)
+                 {
+                     status = "ülikoolilaps";
+                 }
+                 else
+                 {
+                     status = "tööline";
+                 }
+                 return status; }
         }
         public void Tervitamine()
         {
-            Console.WriteLine("Tere! Minu perenimi on "+ perenimi);
-            Console.WriteLine("Ma olen {0} aastat vana, olen {1}, ma olen {2} ", vanus, status, status1);
+            if (perenimi!=null && eesnimi!=null)
+            {
+                Console.WriteLine("Tere! Minu nimi on {0} {1}",eesnimi, perenimi);
+            }
+            else if (perenimi == null && perenimi != null)
+            {
+                Console.WriteLine("Tere! Minu eesnimi on " + eesnimi);
+            }
+            else if (eesnimi==null && eesnimi != null)
+            {
+                Console.WriteLine("Tere! Minu perenimi on " + perenimi);
+            }
+            else
+            {
+                Console.WriteLine("Tere!");
+            }
+            Console.WriteLine("Ma olen {0} aastat vana, olen {1}, ma olen {2} ", vanus, Status, status1);
+        }
+        public double SotsialMaks()
+        {
+            double SotsialMaks = 0;
+            SotsialMaks = palk * 0.33;
+            return SotsialMaks;
         }
     }
 }
